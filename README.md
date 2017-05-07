47edb154d2850b1195240145ae2051408da9201b
Andrew L. Van Slaars <avanslaars@gmail.com>
2017-05-06 21:58:23 -0400

    Added readme and commented store code

---
+++ README.md
@@ -0,0 +1,5 @@
+# Nextjs and Freactal Example
+
+This example demonstrates the use of [freactal](https://github.com/FormidableLabs/freactal#freactal) with [Nextjs](https://github.com/zeit/next.js).
+
+The example is just a counter that maintains state between pages.
--- store.js
+++ store.js
@@ -1,6 +1,13 @@
 import { provideState, injectState } from 'freactal'
 import { compose } from 'ramda'
 
+/*
+This is the key to making this work in Next.
+provideState would normally give each wrapped component its own state
+By putting the state here, and returning a reference to the same object
+from initialState, each page can be wrapped and still reference the
+same state object
+*/
 const init = { counter: 0 }
 
 const stateInitializer = {
